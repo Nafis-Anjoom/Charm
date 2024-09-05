b18df2b392867dce78ba90c19e4b4d3cd2f966ad
Nafis Anjoom <nafisanjoom@gmail.com>
2024-09-04 22:53:00 -0400

    added String methods for all Node types.

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,13 @@
 package ast
 
-import "charm/token"
+import (
+	"bytes"
+	"charm/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+    String() string
 }
 
 type Statement interface {
@@ -28,6 +32,16 @@ func (program *Program) TokenLiteral() string {
 	}
 }
 
+func (program *Program) String() string {
+    var out bytes.Buffer
+
+    for _, s := range program.Statements {
+        out.WriteString(s.String())
+    }
+
+    return out.String()
+}
+
 // implements Expression node because it returns value
 type Identifier struct {
 	Token token.Token
@@ -37,6 +51,9 @@ type Identifier struct {
 func (id *Identifier) expressionNode() {}
 func (id *Identifier) TokenLiteral() string {
 	return id.Token.Literal
+}
+func (id *Identifier) String() string {
+    return id.Value
 }
 
 type LetStatement struct {
@@ -49,13 +66,56 @@ func (ls *LetStatement) statementNode() {}
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
+func (ls *LetStatement) String() string {
+    var out bytes.Buffer
+
+    out.WriteString(ls.TokenLiteral() + " ")
+    out.WriteString(ls.Identifier.String())
+    out.WriteString(" = ")
+
+    if ls.Value != nil {
+        out.WriteString(ls.Value.String())
+    }
+
+    out.WriteString(";")
+    return out.String()
+}
+
 
 type ReturnStatement struct {
 	Token      token.Token
-	Value      Expression
+	ReturnValue      Expression
 }
 
 func (rs *ReturnStatement) statementNode() {}
 func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
+}
+func (rs *ReturnStatement) String() string {
+    var out bytes.Buffer
+
+    out.WriteString(rs.TokenLiteral() + " ")
+    if rs.ReturnValue != nil {
+        out.WriteString(rs.ReturnValue.String())
+    }
+    out.WriteString(";")
+    return out.String()
+}
+
+// Expression statement are special statements. It accomodates expressions written as statements.
+// For example: "3 + x;" is a valid statement.
+type ExpressionStatement struct {
+    Token token.Token
+    Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+func (es *ExpressionStatement) String() string {
+    if es.Expression != nil {
+        return es.Expression.String()
+    }
+    return ""
 }
