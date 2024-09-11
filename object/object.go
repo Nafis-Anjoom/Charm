64af7ca7041e63efe7022ffdf5419807646b5f6d
Nafis Anjoom <nafisanjoom@gmail.com>
2024-09-10 23:00:13 -0400

    fixed Null object value. Added Error object type.

--- object/object.go
+++ object/object.go
@@ -9,6 +9,7 @@ const (
     BOOLEAN_OBJ = "BOOLEAN"
     NULL_OBJ = "NULL"
     RETURN_VALUE_OBJ = "RETURN_VALUE"
+    ERROR_OBJ = "ERROR"
 )
 
 type Object interface {
@@ -37,11 +38,9 @@ func (i *Boolean) Inspect() string {
     return fmt.Sprintf("%t", i.Value)
 }
 
-type Null struct {
-    Value bool
-}
+type Null struct {}
 func (n *Null) Type() ObjectType {
-    return BOOLEAN_OBJ
+    return NULL_OBJ
 }
 func (n *Null) Inspect() string {
     return fmt.Sprintf("null")
@@ -55,4 +54,14 @@ func (rv *ReturnValue) Type() ObjectType {
 }
 func (rv *ReturnValue) Inspect() string {
     return rv.Value.Inspect()
+}
+
+type Error struct {
+    Message string
+}
+func (e *Error) Type() ObjectType {
+    return ERROR_OBJ
+}
+func (e *Error) Inspect() string {
+    return "ERROR:" + e.Message
 }
