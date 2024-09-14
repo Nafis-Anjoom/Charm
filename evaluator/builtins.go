1c7d82cba9ea47f1158aa6da7855ad18f5f89e76
Nafis Anjoom <nafisanjoom@gmail.com>
2024-09-14 17:08:13 -0400

    updated print function to print each argument in the same line delimited by space.

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -105,8 +105,9 @@ var builtins = map[string]*object.Builtin {
     "print": {
         Fn: func(args ...object.Object) object.Object {
             for _, arg := range args {
-                fmt.Println(arg.Inspect())
+                fmt.Print(arg.Inspect(), " ")
             }
+            fmt.Println()
 
             return NULL
         },
